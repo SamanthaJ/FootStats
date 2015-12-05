18c055fd01b9eeb17db5aa955799331cf7a99975
samanthaj <samanthajaromirski@gmail.com>
2015-12-05 14:14:06 -0500

    Generates join table for events and teams

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205185814) do
+ActiveRecord::Schema.define(version: 20151205191321) do
 
   create_table "events", force: :cascade do |t|
     t.string   "type"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20151205185814) do
     t.string   "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "events_teams", id: false, force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "team_id",  null: false
   end
 
   create_table "players", force: :cascade do |t|
