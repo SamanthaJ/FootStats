29e9d5f7804b6d0e2bccda7cf9f66ef1fc3a7165
samanthaj <samanthajaromirski@gmail.com>
2015-12-09 13:45:18 -0500

    Alters player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,10 @@
 class Player < ActiveRecord::Base
   belongs_to :team
   has_many :events
+
+  def as_json (options = nil)
+    super({
+      include: [:team, :events]
+    }.merge(options || {}))
+  end
 end
